b3b9956f1148c4c3f0250156b8478e742955f6d1
tom55dev <thomasbill55dev@gmail.com>
2018-03-12 16:37:51 +0900

    Move whitelisted domain on secrets

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,12 @@
 class ApiController < ActionController::API
-  WHITELISTED_DOMAINS = ['oms-tokyotreat-staging.myshopify.com'].freeze # TODO
-
   before_action :valid_request?
 
   protected
 
   def valid_request?
-    unless request.origin.present? && WHITELISTED_DOMAINS.include?(URI.parse(request.origin).host)
+    whitelisted = Rails.application.secrets.whitelisted_domains
+
+    unless request.origin.present? && whitelisted.include?(URI.parse(request.origin).host)
       render json: { msg: 'Sorry, you don\'t have any access to this websote.' }, status: 403
     end
   end
