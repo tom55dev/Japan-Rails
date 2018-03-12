d77d4bf34c0a5f2f1517ad9d8c77524e5d0372dd
tom55dev <thomasbill55dev@gmail.com>
2018-03-12 16:10:55 +0900

    Configure capistrano for staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,59 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock '~> 3.10.1'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :stages, %w[production staging]
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :application, 'japanhaul-rails'
+set :repo_url, 'git@github.com:movefast-llc/japanhaul-rails.git'
+set :branch, 'feature/wishlists-api'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :user, 'deployer'
+set :deploy_to, "/data/#{fetch(:application)}"
+set :ssh_options, { forward_agent: true }
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :pty, false
+set :keep_releases, 5
 
-# Default value for :pty is false
-# set :pty, true
+set :linked_files, %w{config/secrets.yml config/database.yml}
+set :linked_dirs, %w{log tmp/pids tmp/sockets tmp/cache vendor/bundle public/system public/uploads}
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+# sidekiq settings
+set :sidekiq_config, -> { File.join(current_path, 'config', 'sidekiq.yml') }
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :migration_role, :app
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+  before :start, :make_dirs
+end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :deploy do
+  desc 'Initial deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+after 'deploy:publishing', 'deploy:restart'
+after 'deploy:restart', 'sidekiq:restart'
