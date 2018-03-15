2ee52a23b13d8d2f198f3d916fd7e662ff3dbc18
tom55dev <thomasbill55dev@gmail.com>
2018-03-15 13:54:31 +0900

    Adjust attributes for customer_id

--- app/serializables/serializable_wishlist.rb
+++ app/serializables/serializable_wishlist.rb
@@ -2,9 +2,12 @@ class SerializableWishlist < JSONAPI::Serializable::Resource
   type 'wishlists'
 
   id { @object.token }
-  customer_id { @object.shopify_customer_id }
 
   attributes :name, :wishlist_type
+
+  attribute :customer_id do
+    @object.shopify_customer_id
+  end
 
   has_many :wishlist_items
 end
