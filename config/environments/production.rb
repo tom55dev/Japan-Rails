64764672618de89cfbe85661068aa2d105d5a765
tom55dev <thomasbill55dev@gmail.com>
2018-04-25 17:27:42 +0900

    Force SSL on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.force_ssl = true
+  config.ssl_options = { hsts: { subdomains: false } }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
