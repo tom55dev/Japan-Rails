c24e407e223ed2712c7975b3c794f94d1242e43b
tom55dev <thomasbill55dev@gmail.com>
2018-09-10 16:08:44 +0900

    Removes unneeded commited files

--- fetcher.rb
+++
@@ -1,7 +0,0 @@
-streaks = RewardTemplate.where(name: 'A').flat_map(&:streaks)
-Box.activated.where(streak: streaks).group(:streak_id).count
-
-# D - 184 streaks, highest box activated: 2
-# E - 137 streaks, highest box activated: 2
-# F - 43 streaks, highest box activated: 2
-# A - 149 streaks, highest box activated: 2
