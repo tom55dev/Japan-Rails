6e57080aef654e7c6eebf54fedb5e69b75e9c15a
tom55dev <thomasbill55dev@gmail.com>
2018-09-12 14:05:23 +0900

    Removes unnecessary sessions for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,6 @@ redis_opt = {
   url: 'redis://localhost:6379/0', namespace: "sidekiq_japanhaul-rails_#{Rails.env}"
 }
 
-Sidekiq::Web.set :sessions, domain: 'all'
-
 Sidekiq.configure_server do |config|
   config.redis = redis_opt
 end
