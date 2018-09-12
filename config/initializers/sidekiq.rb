a9d5ff3f43b547d4093dfee0f41a1349af0e85b7
tom55dev <thomasbill55dev@gmail.com>
2018-09-12 11:16:44 +0900

    Namespace redis for staging

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,13 @@
+redis_opt = {
+  url: 'redis://localhost:6379/0', namespace: "sidekiq_japanhaul-rails_#{Rails.env}"
+}
+
+Sidekiq::Web.set :sessions, domain: 'all'
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_opt
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_opt
+end
