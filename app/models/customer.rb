2164a9420d84bca44798c7a7f883417dc5c906da
tom55dev <thomasbill55dev@gmail.com>
2018-11-26 12:07:13 +0900

    Strip emoji characters on customer's name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,10 +4,19 @@ class Customer < ApplicationRecord
 
   validates :remote_id, presence: true, uniqueness: { scope: :shop_id }
 
+  before_validation :strip_emoji
+
   def initials
     [
       first_name.to_s[0].try(:upcase),
       last_name.to_s[0].try(:upcase)
     ].compact.join
+  end
+
+  private
+
+  def strip_emoji
+    self.first_name = StripEmoji.replace(first_name) if first_name?
+    self.last_name = StripEmoji.replace(last_name) if last_name?
   end
 end
