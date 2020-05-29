0e79839eb6aabef7affd881880f05953e90e8c13
tom55dev <thomasbill55dev@gmail.com>
2020-05-29 17:06:50 +0900

    Add styling to thank you page

--- app/controllers/order_shipping_controller.rb
+++ app/controllers/order_shipping_controller.rb
@@ -1,7 +1,9 @@
 class OrderShippingController < ApiController
   def new
-    unless call_api
-      flash[:error] = 'Sorry, your request could not be submitted. Please try again later.'
+    if call_api
+      @message = confirmation_message
+    else
+      @error = 'Sorry, your request could not be submitted. Please try again later.'
     end
 
     render :thankyou
@@ -9,14 +11,22 @@ class OrderShippingController < ApiController
 
   private
 
+  def confirmation_message
+    if params[:cancelling].present?
+      "We've fully refunded and cancelled your order."
+    else
+      "We've refunded you $#{params[:amount]} and your order will be reshipped with #{params[:shipping_method]}"
+    end
+  end
+
   def call_api
-    resp = RestClient.post(
+    RestClient.post(
       api_url,
       order_shipping_params.to_json,
       content_type: :json,
       accept: :json
     )
-    JSON.parse(resp.body)
+    response.status == 200
   rescue RestClient::ExceptionWithResponse => e
     raise e if Rails.env.development?
     Appsignal.set_error(e)
