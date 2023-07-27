83a1f9549ad07148b02ab814f87d24c307b550ef
tom55dev <thomasbill55dev@gmail.com>
2023-07-27 19:01:16 +0900

    Remove meaningless call to LoyaltyLion on JH-rails (#40)
    * Remove LoyaltyLion API call from customer updated.

--- app/controllers/api/loyalty_lion_controller.rb
+++ app/controllers/api/loyalty_lion_controller.rb
@@ -2,11 +2,11 @@ class Api::LoyaltyLionController < ApiController
   def customer_updated
     customer_params = params[:payload][:customer]
 
-    LoyaltyLion::PointsUpdateJob.perform_later(
-      shop: params[:shop],
-      customer_id: customer_params[:merchant_id],
-      points_approved: customer_params[:points_approved]
-    )
+    # LoyaltyLion::PointsUpdateJob.perform_later(
+    #   shop: params[:shop],
+    #   customer_id: customer_params[:merchant_id],
+    #   points_approved: customer_params[:points_approved]
+    # )
 
     head :ok
   end
